c884217aa1e8a3a9fb8a20da807c61ddde632c53
osama aref <zoro9483@gmail.com>
2020-09-17 14:27:47 +0200

    editing persons js file

--- section-7/src/components/Persons/Person/Person.js
+++ section-7/src/components/Persons/Person/Person.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./Person.css";
+// import Aux from "../../../HOC/Auxiliary";
+import PropTypes from "prop-types";
+import AuthContext from "../../../context/auth-context";
 
-import Person from '.Preson/Person'
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
 
-const Person = (props) => props.persons.map( ( person, index ) =>
-     return <Person
-     click={() => props.clicked( index )}
-           name={person.name}
-           age={person.age}
-           key={person.id}
-           change={( event )} => props.changed( event, person.id )}
-          } );
+  static contextType = AuthContext;
 
-export default persons;
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        {this.context.authenticated ? (
+          <p>Authenticated</p>
+        ) : (
+          <p>Please log in</p>
+        )}
+        {/* <AuthContext.Consumer>
+          {context => {
+          }}
+        </AuthContext.Consumer> */}
+        {/* // <div className="Person"> */}
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old
+        </p>
+        <p>{this.props.children}</p>,
+        <input
+          type="text"
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </React.Fragment>
+      // {/* </div> */}
+    );
+  }
+}
 
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
+export default Person;
 
 
 
@@ -78,61 +120,5 @@ export default persons;
 
 
 
-// import React, { Component } from "react";
-// import "./Person.css";
-// // import Aux from "../../../HOC/Auxiliary";
-// import PropTypes from "prop-types";
-// import AuthContext from "../../../context/auth-context";
 
-// class Person extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.inputElementRef = React.createRef();
-//   }
 
-//   static contextType = AuthContext;
-
-//   componentDidMount() {
-//     this.inputElementRef.current.focus();
-//     console.log(this.context.authenticated);
-//   }
-//   render() {
-//     return (
-//       <React.Fragment>
-//         {this.context.authenticated ? (
-//           <p>Authenticated</p>
-//         ) : (
-//           <p>Please log in</p>
-//         )}
-//         {/* <AuthContext.Consumer>
-//           {context => {
-//           }}
-//         </AuthContext.Consumer> */}
-//         {/* // <div className="Person"> */}
-//         <p onClick={this.props.click}>
-//           I'm {this.props.name} and I am {this.props.age} years old
-//         </p>
-//         <p>{this.props.children}</p>,
-//         <input
-//           type="text"
-//           // ref={inputEl => {
-//           //   this.inputElement = inputEl;
-//           // }}
-//           ref={this.inputElementRef}
-//           onChange={this.props.changed}
-//           value={this.props.name}
-//         />
-//       </React.Fragment>
-//       // {/* </div> */}
-//     );
-//   }
-// }
-
-// Person.propTypes = {
-//   click: PropTypes.func,
-//   name: PropTypes.string,
-//   age: PropTypes.number,
-//   changed: PropTypes.func
-// };
-
-// export default Person;
